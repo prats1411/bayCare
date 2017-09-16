2c13c759499a3cfeef377a296f8a941a0b265b3b
Deeksha Bajaj <dishab16@gmail.com>
2017-09-16 23:14:11 +0900

    dashboard

--- admin/list.php
+++ admin/list.php
@@ -25,7 +25,6 @@ $companies = $statement->fetchAll();
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
     <link href="css/animate.css" rel="stylesheet">
-    <link href="css/bootstrap-toggle.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <link href="css/plugins/iCheck/custom.css" rel="stylesheet">
 
